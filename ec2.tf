fb6d4996ffeeff1722b9311b583506d514ae4fe8
chandhusane1234 <141405992+chandhusane1234@users.noreply.github.com>
2023-08-28 22:14:09 +0530

    done

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0f34c5ae932e6f0e4" 
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.privatesubnets.id
+  root_block_device {
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = aws_kms_key.my_kms_key.key_id
+    delete_on_termination = true
+  }
+}
+
+resource "aws_security_group_rule" "example_ssh_rule" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.1.0/24"]
+  security_group_id = aws_kms_key.my_kms_key.key_id
+}
---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    access_key = "AKIA2YTIXAIE3VUMICG4"
+    secret_key = "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"
+    region = "us-east-1"
+}
+resource "aws_iam_user" "chandhusane123" {
+    name = "chandhusane123"
+}
